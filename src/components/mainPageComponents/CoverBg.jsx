af4d6a5cf95218f9ca71ec15ffa75ab619f58e43
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-04-08 01:11:10 +0630

    Text added

--- src/components/mainPageComponents/CoverBg.jsx
+++ src/components/mainPageComponents/CoverBg.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CoverBg = () => {
   return (
-    <div className=" w-full h-[463px] flex flex-col justify-center items-center">
-      <h1>Hello I'm here</h1>
-      <h1>Ha Ha Ha</h1>
+    <div className=" w-full h-[460px] flex flex-col justify-center items-center gap-5">
+      <h1 className=' mainText text-mainText-light dark:text-mainText-dark'>Hello Welcome To My Portfolio Website .</h1>
+      <h1 className=' subTitle text-greyText-light dark:text-greyText-dark'>Dive into my portfolio to see my passion in action, and don't hesitate to connect with me!</h1>
     </div>
 
 
@@ -12,3 +12,6 @@ const CoverBg = () => {
 }
 
 export default CoverBg
+
+
+// Greetings! I'm [Your Name], a Frontend Web Developer who believes in the power of visual storytelling. With a blend of modern web technologies, I craft digital experiences that are not only functional and responsive, but also engaging and visually compelling. I'm a problem solver at heart, with a keen eye for design detail and a relentless pursuit of pixel perfection. Dive into my portfolio to see my passion in action, and don't hesitate to connect with me!
--- src/components/mainPageComponents/HeroSection.jsx
+++ src/components/mainPageComponents/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <div className=' w-full bg-gradient-to-t from-white to-[#242526] dark:from-[#242526] dark:to-transparent'>
       <div className=' flex justify-center'>
-        <div className=" lg:w-[1250px] md:w-full sm:w-full h-full bg-gradient-to-b from-white to-[#242526]">
+        <div className=" lg:w-[1250px] md:w-full sm:w-full h-full bg-gradient-to-t from-white to-[#242526] dark:from-[#242526] dark:to-white">
           <CoverBg />
         </div>
       </div>
