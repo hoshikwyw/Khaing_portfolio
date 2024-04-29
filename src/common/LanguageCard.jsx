5ef090d5ef7907b85a5b8cbaf524326edcd41fae
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-04-29 22:28:04 +0630

    Test

--- src/common/LanguageCard.jsx
+++ src/common/LanguageCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const LanguageCard = ({src , text}) => {
+const LanguageCard = ({ src, text }) => {
+  console.log(src);
   return (
-    <div>
-      <img src={src} alt="" />
-      <h1>{text}</h1>
+    <div className=' w-[100px] h-[120px] bg-[#7E8080] dark:bg-[#CBCCCC] relative rounded-md'>
+      <img src={src} alt="" className=' w-full h-full' />
+      <h1 className=' absolute'>{text}</h1>
     </div>
   )
 }
--- src/components/mainPageComponents/LanguagesIntro.jsx
+++ src/components/mainPageComponents/LanguagesIntro.jsx
@@ -6,7 +6,7 @@ const LanguagesIntro = () => {
     <div className=' w-full px-3 py-2'>
       <h1 className=' subTitle text-gray-500 dark:text-gray-200'>Languages</h1>
       <div className="container">
-        <LanguageCard src="" text="" />
+        <LanguageCard src="/html.png" text="HTML" />
       </div>
     </div>
   )
