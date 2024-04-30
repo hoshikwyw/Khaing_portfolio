db91f5fa7bb343c98d1f2f32c84c6c69e9147f87
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-04-30 16:03:32 +0630

    Languages added

--- src/components/mainPageComponents/LanguagesIntro.jsx
+++ src/components/mainPageComponents/LanguagesIntro.jsx
@@ -3,10 +3,18 @@ import LanguageCard from '../../common/LanguageCard'
 
 const LanguagesIntro = () => {
   return (
-    <div className=' w-full px-3 py-2'>
-      <h1 className=' subTitle text-gray-500 dark:text-gray-200'>Languages</h1>
-      <div className="container">
-        <LanguageCard src="/html.png" text="HTML" />
+    <div className=' w-full px-3 py-2 flex flex-col justify-center items-center'>
+      <div className=' flex w-full justify-between items-center'>
+        <h1 className=' subTitle text-gray-500 dark:text-gray-200'>Languages</h1>
+        <button className=' smallText dark:text-[#c1bfbf]'>See More</button>
+      </div>
+      <div className="container mt-2 flex flex-wrap justify-center items-center p-3 gap-7">
+        <LanguageCard src="/logos/html.png" text="HTML" />
+        <LanguageCard src="/logos/css.png" text="CSS" />
+        <LanguageCard src="/logos/js.png" text="JS" />
+        <LanguageCard src="/logos/sass.png" text="SASS" />
+        <LanguageCard src="/logos/react.png" text="REACT" />
+        <LanguageCard src="/logos/git.png" text="GIT" />
       </div>
     </div>
   )
