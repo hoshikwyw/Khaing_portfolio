196348153c9d1b35612f08330c62cfa2be6bdb9f
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-05-02 07:59:49 +0630

    Experience Added

---
+++ src/components/mainPageComponents/IntroExp.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SeeMorebtn from '../../common/SeeMorebtn'
+import IntroList from '../../common/IntroList'
+import { IoIosSchool, IoIosHome } from "react-icons/io";
+import { IoLocationSharp } from "react-icons/io5";
+import Button from '../../common/Button';
+
+const IntroExp = () => {
+    return (
+        <div className=' w-full px-3 py-2 flex flex-col'>
+            <div className=' flex w-full justify-between items-center'>
+                <h1 className=' subTitle text-gray-500 dark:text-gray-200'>Experience</h1>
+                <SeeMorebtn text="See More Details" />
+            </div>
+            <div className=' flex flex-col gap-3 my-3'>
+                <IntroList icon={IoIosSchool} text="Collabration with team" />
+                <IntroList icon={IoIosSchool} text="Workshop with Frontend Web Dev Position at MMS-IT" />
+                <IntroList icon={IoIosHome} text="Junior Frontend Web Dev at IT-Wizard Myanmar" />
+                <IntroList icon={IoIosHome} text="Freelance As Frontend Web Dev at Upwork" />
+            </div>
+        </div>
+    )
+}
+
+export default IntroExp
