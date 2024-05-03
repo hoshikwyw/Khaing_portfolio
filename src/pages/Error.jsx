3c934faa627cb186532d2c51c138abed2d39bb81
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-05-03 13:27:31 +0630

    Error Img change

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,8 +5,8 @@ import errorAnimation from "../lotties/error.json"
 const Error = () => {
   return (
     <div className=' w-screen h-screen flex justify-center items-center flex-col'>
-      <h1 className=' font-bold text-8xl'>I'm the error</h1>
-      <Lottie animationData={errorAnimation} loop={true} />
+      {/* <Lottie animationData={errorAnimation} loop={true} /> */}
+      <img src="/errorImg.jpg" alt="" className=' w-[50%]' />
     </div>
   )
 }
